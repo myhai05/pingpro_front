e2013fde99581a9eb3a0031fec15960c4a8acba9
myhai05 <mmishk0501@gmail.com>
2024-08-29 14:37:30 +0200

    V.9.1.1

--- src/components/Auth/logout.js
+++ src/components/Auth/logout.js
@@ -3,28 +3,24 @@ import axios from 'axios';
 import { AuthContext } from '../Context/authContext';
 import Cookies from 'js-cookie';
 
-
-
-
 const Logout = () => {
 
   const { setUser } = useContext(AuthContext);
-
-  const removeCookie = (key) => {
+/*  const removeCookie = (key) => {
     if (window != "undefined") {
       Cookies.remove(key, { expires: 1 });
     }
-  };
+  };*/
 
   const logout = async () => {
     try {
       await axios({
-        method: "get",   
+        method: "get",
         url: `${process.env.REACT_APP_API_URL}api/logout`,
         withCredentials: true,
       });
-     
-      removeCookie("jwt");
+
+      //removeCookie("jwt");
       setUser(null);
     } catch (err) {
       console.log(err);
