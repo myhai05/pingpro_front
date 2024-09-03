e424b1603ba969a745fbf5098b77c6f02f885c87
myhai05 <mmishk0501@gmail.com>
2024-09-03 21:22:01 +0200

    mentions added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ import Logout from './Auth/logout';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Dashboard from './Dashboard/dashboard';
+
 
 const Header = () => {
   const { user } = useContext(AuthContext); // Utiliser le contexte pour obtenir l'utilisateur
